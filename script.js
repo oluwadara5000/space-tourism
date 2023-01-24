9901d4dee4cfe953251cecbb8a2b79474249d281
Oluwadarasimi Odejobi <oluwadarasimi5000@gmail.com>
2023-01-24 22:50:04 +0100

    started the media queries for destination page

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // START DEVELOPING THE MENU PAGE AND HAMBURGER MENU
+const openMenu = document.querySelector("#nav-bar");
+const closeMenu = document.querySelector("#cancel");
+const panel = document.querySelector("#panel");
+
+document.getElementById("cancel").style.display = "none";
+document.getElementById("nav-list").style.display = "none";
+document.getElementById("panel").style.padding = "0"
+
+openMenu.addEventListener("click", function () {
+  document.getElementById("panel").style.width = "275px";
+  document.getElementById("cancel").style.display = "block";
+  document.getElementById("nav-list").style.display = "block";
+  document.getElementById("panel").style.padding = "34px 27px 0 32px";
+})
+
+closeMenu.addEventListener("click", function () {
+  document.getElementById("panel").style.width = "0"
+  document.getElementById("cancel").style.display = "none";
+  document.getElementById("nav-list").style.display = "none";
+  document.getElementById("panel").style.padding = "0";
+})
