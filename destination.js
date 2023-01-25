d000f4361e6ea932a463d4f6917a26340d9a44b1
Oluwadarasimi Odejobi <oluwadarasimi5000@gmail.com>
2023-01-25 22:53:25 +0100

    finished media query for destination page. Crew page up next

--- destination.js
+++ destination.js
@@ -1,3 +1,16 @@
+function myFunction(x) {
+  if (x.matches) {
+    document.getElementById("nav-bar").style.display = "block";
+  } else {
+    document.getElementById("nav-bar").style.display = "none";
+  }
+}
+
+var x = window.matchMedia("(max-width: 767px)");
+myFunction(x); // Call listener function at run time
+x.addListener(myFunction); // Attach listener function on state changes
+
+
 // START DEVELOPING THE MENU PAGE AND HAMBURGER MENU
 const openMenu = document.querySelector("#nav-bar");
 const closeMenu = document.querySelector("#cancel");
@@ -11,9 +24,10 @@ openMenu.addEventListener("click", function () {
   document.getElementById("nav-bar").style.display = "none";
   document.getElementById("panel").style.width = "275px";
   document.getElementById("cancel").style.display = "block";
+  document.getElementById("header").style.width = "50%";
   document.getElementById("cancel").style.margin = "0";
   document.getElementById("nav-list").style.display = "block";
-  document.getElementById("panel").style.padding = "40px 45px 0 32px";
+  document.getElementById("panel").style.padding = "50px 27px 0 32px";
 });
 
 closeMenu.addEventListener("click", function () {
@@ -22,5 +36,5 @@ closeMenu.addEventListener("click", function () {
   document.getElementById("nav-list").style.display = "none";
   document.getElementById("panel").style.padding = "0";
   document.getElementById("nav-bar").style.display = "block";
-  document.getElementById("header").style.width = "100%"
+  document.getElementById("header").style.width = "100%";
 });
